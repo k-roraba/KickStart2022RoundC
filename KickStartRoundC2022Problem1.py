72876284a063763c5d9fcf1c10c214fa9815ff0e
K. Rorabaugh <102931841+k-roraba@users.noreply.github.com>
2022-05-22 17:19:50 -0500

    Update KickStartRoundC2022Problem1.py

--- KickStartRoundC2022Problem1.py
+++ KickStartRoundC2022Problem1.py
@@ -1,5 +1,9 @@
 #CodeJam 2022 Round C Problem 1 - New Password
 
+# this could've taken me 30 minutes, if I hadn't used '==' when I meant '='.  
+# Something about fiddling with booleans makes me want to use the '=='.
+# Took me about 20 minutes to realize what I had done.
+
 cases = int(input())
 output = []
 
@@ -10,28 +14,25 @@ for i in range(cases):
     containsInt = False
     containsSpecial = False
     containsUpper = False
-    containsLower = False
-    
+    containsLower = False   
 
     for j in pwd:
         #check if 1 digit; can I turn this off once found?
         if not containsInt and j.isdigit():
             #print(j + " is digit")
             containsInt = True
-        
-            
+            # I tried a break statement here, but that made the thing quit way too soon;
+            # I never truly learned what 'break' does in a nested loop situation   
 
         #check if one uppercase
         if not containsUpper and j.isupper():
-            #print(j + " is uppercase")
-            containsUpper = True
-        
+            #print(j + " is uppercase") -- was using this when the break statement was skipping stuff
+            containsUpper = True 
 
         #check if one lowercase
         if not containsLower and j.islower():
             #print(j + " is lowercase")
             containsLower = True
-        
 
         #check if special char # @ * and & 
         if not containsSpecial:
